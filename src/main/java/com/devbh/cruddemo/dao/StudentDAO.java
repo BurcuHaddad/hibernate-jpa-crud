42bfb0258c683cdd68765be4d0d8d3656c51f95e
BurcuHaddad <burcuhaddad@gmail.com>
2024-01-11 10:27:44 +0300

    create read and update

---
+++ src/main/java/com/devbh/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,19 @@
+package com.devbh.cruddemo.dao;
+
+import com.devbh.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+
+}
